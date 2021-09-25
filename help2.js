061e227e66bf8e13175c2a34919b50c9f5232a5f
Younes A <91132589+a6younes@users.noreply.github.com>
2021-09-26 01:07:20 +0200

    Add files via upload

---
+++ help2.js
@@ -0,0 +1,38 @@
+//############################################################################################//
+//auteur : ASSAOUCI Younes//
+// date creation : 25/09/21//
+// script : help2.js//
+// permet de creer des couples a partir d'un nombre determiné d'hommes et de femmes//
+//############################################################################################//
+
+
+function min (a ,b ) {
+    if(a>b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+
+
+
+
+const readline = require('readline');
+
+//Le readlinemodule fournit une interface pour lire les données d'un flux lisible //
+
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('nb homme ? ', (answer1) => {
+    rl.question('nb femme ? ', (answer2) => {
+        var result = min(answer1,answer2);   // ligne divisant le resultat de l'addition par 2 et obtenir un nombre entier.
+        console.log(`Le nombre de couple est de : ${'👫'.repeat (result)} `);
+        rl.close();
+    });
+});
