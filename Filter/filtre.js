f5fafdbf06675bc08002b44a92325b0ffb12c061
Younes A <91132589+a6younes@users.noreply.github.com>
2021-10-11 22:20:56 +0200

    Update filtre.js

--- Filter/filtre.js
+++ Filter/filtre.js
@@ -1,1 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////
+////AUTEUR : ASSAOUCI YOUNES
+/////////////////////////////////////////////////////////////////////////////
+
+// importation 
+
+let fonctions = require('./fonctions');
+
+
+
+// constantes
+
+const Pizzas = [
+
+    { name:"queen", w: ["🐷","🍄","🍅","🧀"] },
+
+    { name: "cheese", w: ["🧀", "🍅"]},
+
+    { name: "oriental", w: ["🍅","🐑","🍄","🌶"]},
+
+    { name: "royal", w: ["🍅","🌵"]},
+
+  ],
+
+
+
+  price = new Map([ ["🍅", 1], ["🐷", 2], ["🌶",2], ["🍄", 5], ["🧀", 5], ["🐑", 2], ["🌵", 10]]);
+
+
+//Donner l'ensemble des ingrédients
+
+let ingredients = Pizzas.filter(function(Pizzas) {
+    
+   console.log(Pizzas.w)
+    
+}) 
+
+
+//Donner pour chaque ingrédient le nombre de pizzas 
+
+
 
