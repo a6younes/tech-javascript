6f8dbfc427ad371a83a2fbedb60811cc4ffde28b
Younes A <91132589+a6younes@users.noreply.github.com>
2021-10-11 21:44:21 +0200

    Update fonctions.js

--- foreach/fonctions.js
+++ foreach/fonctions.js
@@ -1,28 +1,20 @@
 
 //Definition
 
-
-
 function forEach(t, fonct) {
     for (let i = 0; i < t.length; i++) {
         fonct(t[i])
+        
     }
   }
 
 
 
   function affVal(v){
-    console.log(`valeurs du tableau : ${v}`)
+    console.log(`valeurs du tableau : ${v} \n`)
  }
  
     
-
-
-
-
-
-
-
 // Exportation
-module.exports.bouclef = forEach
-module.exports.funaff = affVal
+module.exports.boucle = forEach
+module.exports.affichage = affVal
